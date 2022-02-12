bcd1ff1f8672379ecbe12b47769d2fe2119d4741
Chloe Kever <chloe.kever@platonic.systems>
2022-02-11 18:42:23 -0800

    Add dry runs.

--- flake.nix
+++ flake.nix
@@ -11,36 +11,42 @@
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
     in rec
-    { 
+    {
       overlay = final: prev: {
         generateApps = flake:
           let
             machines = builtins.attrNames flake.nixosConfigurations;
             validMachines = final.lib.remove "" (final.lib.forEach machines (x: final.lib.optionalString (flake.nixosConfigurations."${x}"._module.args ? nixinate) "${x}" ));
-            mkDeployScript = machine: final.writeScript "deploy-${machine}.sh" ''
-              set -e
-              SSH_USER=${flake.nixosConfigurations.${machine}._module.args.nixinate.sshUser}
-              SSH_HOST=${flake.nixosConfigurations.${machine}._module.args.nixinate.host}
-              BUILD_ON=${flake.nixosConfigurations.${machine}._module.args.nixinate.buildOn}
-              SYSTEM_CLOSURE=${flake}#nixosConfigurations.${machine}.config.system.build.toplevel
-              
-              echo "🚀 Deploying nixosConfigurations.${machine} from ${flake}"
-              echo "👤 SSH User: $SSH_USER"
-              echo "🌐 SSH Host: $SSH_HOST"
-              if [ $BUILD_ON = "remote" ]; then
+            mkDeployScript = dry: machine: let
+              inherit (builtins) abort;
+
+              n = flake.nixosConfigurations.${machine}._module.args.nixinate;
+              user = n.sshUser or "root";
+              host = n.host;
+              closure = "${flake}#nixosConfigurations.${machine}.config.system.build.toplevel";
+              where = n.buildOn or "remote";
+              remote = if where == "remote" then true else if where == "local" then false else abort "_module.args.nixinate.buildOn is not set to a valid value of 'local' or 'remote'";
+              switch = if dry then "dry-activate" else "switch";
+              script = (if remote then ''
                 echo "🚀 Sending flake to ${machine} via rsync:"
-                ( set -x; ${final.rsync}/bin/rsync -q -vz --recursive --zc=zstd ${flake}/* $SSH_USER@$SSH_HOST:/tmp/nixcfg/ )
+                ( set -x; ${final.rsync}/bin/rsync -q -vz --recursive --zc=zstd ${flake}/* ${user}@${host}:/tmp/nixcfg/ )
                 echo "🤞 Activating configuration on ${machine} via ssh:"
-                ( set -x; ${final.openssh}/bin/ssh -t $SSH_USER@$SSH_HOST 'sudo nixos-rebuild switch --flake /tmp/nixcfg#${machine}' )
-              elif [ $BUILD_ON = "local" ]; then
+                ( set -x; ${final.openssh}/bin/ssh -t ${user}@${host} 'sudo nixos-rebuild ${switch} --flake /tmp/nixcfg#${machine}' )
+              '' else ''
                 echo "🔨 Building system closure locally and copying it to remote store:"
-                ( set -x; ${final.nixFlakes}/bin/nix copy --to ssh://$SSH_USER@$SSH_HOST $SYSTEM_CLOSURE )
+                ( set -x; ${final.nixFlakes}/bin/nix copy --to ssh://${user}@${host} ${closure} )
                 echo "🤞 Activating configuration on ${machine} via ssh:"
-                SYSTEM_CLOSURE_PATH=$(${final.nixFlakes}/bin/nix path-info $SYSTEM_CLOSURE)
-                ( set -x; ${final.openssh}/bin/ssh -t $SSH_USER@$SSH_HOST "sudo $SYSTEM_CLOSURE_PATH/bin/switch-to-configuration switch" )
-              else
-                echo "_module.args.nixinate.buildOn is not set to a valid value of 'local' or 'remote'"
-              fi
+                SYSTEM_CLOSURE_PATH=$(${final.nixFlakes}/bin/nix path-info ${closure})
+                ( set -x; ${final.openssh}/bin/ssh -t ${user}@${host} "sudo $SYSTEM_CLOSURE_PATH/bin/switch-to-configuration ${switch}" )
+              '') + ''
+                echo "🚀 Deploying nixosConfigurations.${machine} from ${flake}"
+                echo "👤 SSH User: ${user}"
+                echo "🌐 SSH Host: ${host}"
+              '';
+            in final.writeScript "deploy-${machine}.sh" ''
+              set -e
+              SYSTEM_CLOSURE=${flake}#nixosConfigurations.${machine}.config.system.build.toplevel
+              ${script}
             '';
           in
           {
@@ -48,16 +54,24 @@
                (
                  nixpkgs.lib.genAttrs
                    validMachines
-                   (x: 
-                     { 
+                   (x:
+                     {
                        type = "app";
-                       program = toString (mkDeployScript x);
+                       program = toString (mkDeployScript false x);
                      }
                    )
+                   // nixpkgs.lib.genAttrs
+                      (map (a: a ++ "-dry-run") validMachines)
+                      (x:
+                        {
+                          type = "app";
+                          program = toString (mkDeployScript true x);
+                        }
+                      )
                );
           };
         };
       nixinate = forAllSystems (system: nixpkgsFor.${system}.generateApps);
       apps = nixinate.x86_64-linux examples;
     };
-}     
+}
